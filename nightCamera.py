6fdf273aafd964ecd9f38b9416a77fee682fd972
岩井聡 <iwaisatoshi@iwaisatoshi-no-MacBook-Air.local>
2016-03-09 18:33:25 +0900

    command line workload reduction - setting duration feature - set interval default to 60 secs (1 minutes)

--- nightCamera.py
+++ nightCamera.py
@@ -10,31 +10,30 @@ import os
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument('-e','--endHour', help="time the capture ends(24 hour)", type=int)
-    p.add_argument('-i','--interval', help="capture interval time(sec) >3 sec", type=int)
-    p.add_argument('-d','--directory', help="directory to save jpg files");
+    p.add_argument('-d','--dHour', help="duration of the capture (hour)", type=int)
+    p.add_argument('-i','--interval', help="capture interval time(sec) >3 sec", type=int, default=60)
     args = p.parse_args()
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(10,GPIO.OUT)
 #    print args.endHour
     GPIO.output(10,True)
-    dirname = args.directory
+    dirname = datetime.date.today().strftime("%y%m%d")
+    stopTime = datetime.datetime.now() + datetime.timedelta(hours=args.dHour)
     try:
         os.mkdir(dirname)
     except:
         pass
     with picamera.PiCamera() as camera:
         try:
-            for i, filename in enumerate(camera.capture_continuous(dirname+'/im{timestamp:%m%d%H%M%S}-{counter:03d}.jpg')):
+            for i, filename in enumerate(camera.capture_continuous(dirname+'/im{timestamp:%H%M%S}-{counter:03d}.jpg')):
                 print(filename)
                 time.sleep(3)
                 GPIO.output(10,False)
-                nowtime = datetime.datetime.now()
-                if nowtime.hour > args.endHour:
+                nowTime = datetime.datetime.now()
+                if stopTime-nowTime > 0:
                     break
                 time.sleep(args.interval-6)
                 GPIO.output(10,True)
                 time.sleep(3)
         finally:
             pass
-
